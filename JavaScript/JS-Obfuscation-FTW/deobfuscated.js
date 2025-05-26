3459a8598db58d468af7b66f374166bdfb7f73fe
Ashley Jordan Chihiya <104660926+ajchihiyah@users.noreply.github.com>
2025-05-26 20:28:33 +0200

    Delete JavaScript directory

--- JavaScript/JS-Obfuscation-FTW/deobfuscated.js
+++
@@ -1,9 +0,0 @@
-if (document.getElementById("pass").value=="j00w1n")
-{
-    alert("You WIN!");
-    window.location += "?lvl_password="+document.getElementById("pass").value
-}
-else
-{
-    alert("WRONG! Try again!")
-}
